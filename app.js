09774c596155f2651c906c6e34d4c4e8d9ee9662
dodkipod <adamlr89@gmail.com>
2021-02-23 23:24:02 +0200

    goddamn

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.use('/cards', cards);
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, 'client/build')));
 
-  app.get('/', function(req, res) {
+  app.get('/*', function(req, res) {
     res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
   });
 }
