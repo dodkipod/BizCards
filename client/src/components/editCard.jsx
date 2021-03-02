5d91b7bae58082c616fd401a730d8f5592189fe1
dodkipod <adamlr89@gmail.com>
2021-03-02 17:23:32 +0200

    added a log in editcard component to look at what returns from the server

--- client/src/components/editCard.jsx
+++ client/src/components/editCard.jsx
@@ -31,6 +31,9 @@ class EditCard extends Form {
   async componentDidMount(){
     const cardId = this.props.match.params.id;
     const {data}=await cardService.getCard(cardId);
+    // hey
+    console.log(data);
+    //----------------
     this.setState({data:this.mapToViewModel(data)});
   }
 
